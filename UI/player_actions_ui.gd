7de1c3265d17ffcd4adc07a3f96230237b897ff8
Patrick Brochu <patrickbrochu70@gmail.com>
2026-04-08 08:38:03 -0400

    Finally fixed the problem with the playerAction UI, its now toggling on and off visibility and positionned right

--- UI/player_actions_ui.gd
+++ UI/player_actions_ui.gd
@@ -5,7 +5,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	attackBtn.pressed.connect(attackIsPressed)
+	attackBtn.pressed.connect(Callable(self, "attackIsPressed"))
+	print("Connect")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
