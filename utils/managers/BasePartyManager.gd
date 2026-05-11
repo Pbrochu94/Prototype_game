3f47871987ab9d742d82960efd97ef6ba1ea922f
Patrick Brochu <patrickbrochu70@gmail.com>
2026-05-11 16:19:47 -0400

    Player manager successfully herited from base party script now to implement enmus

---
+++ utils/managers/BasePartyManager.gd
@@ -0,0 +1,54 @@
+extends Node
+class_name BasePartyManager
+
+
+#NODES
+@onready var turnManager = get_tree().get_first_node_in_group("turn manager")
+#VARIABLES
+var party:Array[Node2D]
+var aliveCount:int
+var currentlyAliveCharacters:Array[Node2D]
+
+#SIGNALS
+signal partyDead
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	loadRandomTeam()
+	connectSignals()
+	currentlyAliveCharacters = party
+
+#INIT
+func connectSignals():
+	turnManager.playOutroAnim.connect(outroAnim)
+#PARTY HANDLERS
+func onCharacterDeath(character:Node2D):
+	aliveCount -= 1
+	currentlyAliveCharacters = party.filter(
+		func(character):
+			return not character.isDead
+	)
+	print("Remaining party characters alive : ", currentlyAliveCharacters)
+	if aliveCount <= 0:
+		turnManager.fightIsOver = true
+		turnManager.playerLost = true
+func outroAnim():
+	for character in currentlyAliveCharacters:
+		character.setState("outro")
+#TEST DATA
+var allCharacters:Array[PackedScene] = [
+	preload("res://Invocations/Samurai/SamuraiScene.tscn"),
+	preload("res://Invocations/CannonDruid/CannonDruidCombat.tscn"),
+	preload("res://Invocations/Archer/ArcherCombat.tscn"),
+	preload("res://Invocations/BlasterDruid/BlasterDruidCombat.tscn")
+]
+func loadRandomTeam():
+	for i in range(1):
+		#Random characters
+#		var character = allCharacters.pick_random().instantiate()
+		#Specific character to test
+		var character = allCharacters[3].instantiate()
+		character.faction = character.Faction.SUMMON
+		character.isDowned.connect(onCharacterDeath)
+		aliveCount += 1
+		party.append(character)
