c10c5812cffe6c80f5205d2604452edc60bf6bf6
Patrick Brochu <patrickbrochu70@gmail.com>
2026-05-11 17:27:31 -0400

    Both party herited from BaseParty manager, now to work on implementing EVERY enum into enum file

--- utils/managers/enemyPartyManager.gd
+++ utils/managers/enemyPartyManager.gd
@@ -1,54 +1,6 @@
-extends Node
+extends BasePartyManager
 
-#NODES
-@onready var turnManager = get_tree().get_first_node_in_group("turn manager")
-#VARIABLES
-var party:Array[Node2D]
-var aliveCount:int
-var currentlyAliveCharacters:Array[Node2D]
-
-#SIGNALS
-signal partyDead
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	super()
+	partyFaction = Enum.Faction.ENEMY
 	loadRandomTeam()
-	connectSignals()
-	currentlyAliveCharacters = party
-
-#INIT
-func connectSignals():
-	turnManager.playOutroAnim.connect(outroAnim)
-
-#PARTY HANDLERS
-func onCharacterDeath(character:Node2D):
-	aliveCount -= 1
-	currentlyAliveCharacters = party.filter(
-		func(character):
-			return not character.isDead
-	)
-	print("Currently alive enemies: ", currentlyAliveCharacters)
-	if aliveCount <= 0:
-		turnManager.fightIsOver = true
-		turnManager.playerWon = true
-func outroAnim():
-	for character in currentlyAliveCharacters:
-		character.setState("outro")
-
-#TEST DATA
-var allCharacters:Array[PackedScene] = [
-	preload("res://Invocations/Samurai/SamuraiScene.tscn"),
-	preload("res://Invocations/CannonDruid/CannonDruidCombat.tscn"),
-	preload("res://Invocations/Archer/ArcherCombat.tscn"),
-	preload("res://Invocations/BlasterDruid/BlasterDruidCombat.tscn")
-]
-func loadRandomTeam():
-	for i in range(1):
-		#Random characters
-#		var character = allCharacters.pick_random().instantiate()
-		#Specific character to test
-		var character = allCharacters[3].instantiate()
-		character.faction = Enum.Faction.ENEMY
-		character.isDowned.connect(onCharacterDeath)
-		aliveCount += 1
-		party.append(character)
