fbcec99fe5a6d73f362f54750b46aba53653e946
Patrick Brochu <patrickbrochu70@gmail.com>
2026-05-20 15:35:11 -0400

    shield appears centered and active, now need to add dfuration to unit effects

--- Summoner/Spells/BaseSpellVisualScript.gd
+++ Summoner/Spells/BaseSpellVisualScript.gd
@@ -2,14 +2,13 @@ extends Node2D
 class_name BaseSpellScript
 @onready var anim = $SpritePivot/AnimatedSprite2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	anim.animation_finished.connect(onAnimationFinished)
 	anim.play("appear")
 
-
 func onAnimationFinished():
-	match anim:
+	match anim.animation:
 		"appear":
 			anim.play("active")
 		"disapear":
