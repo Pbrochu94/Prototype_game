9ff667f5067ecdd6f258d8dceb27f79880594858
Patrick Brochu <patrickbrochu70@gmail.com>
2026-06-12 09:46:15 -0400

    Added white shadder to archer that works, now moving to fire lord and check if can hurt during attack

--- utils/States/Combat/Hurt.gd
+++ utils/States/Combat/Hurt.gd
@@ -5,7 +5,8 @@ func _onready():
 	pass
 
 func enter():
-	owner.anim.play("hurt")
+#	owner.anim.play("hurt")
+	hurtFlash()
 
 func update(delta):
 	pass
@@ -14,3 +15,10 @@ func exit():
 	pass
 
 
+func hurtFlash():
+	var mat = owner.anim.material as ShaderMaterial
+	mat.set_shader_parameter("flash_amount", 1.0)
+	var tween = create_tween()
+	tween.tween_method(
+		func(value):
+			mat.set_shader_parameter("flash_amount", value),1.0,0.0,0.25)
