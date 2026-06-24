570ff0c764c107bcc17e0a4dc351bc65841413ef
Patrick Brochu <patrickbrochu70@gmail.com>
2026-06-24 17:13:34 -0400

    All attacks successfully moved to stats instead of static

--- Invocations/CannonDruid/BaseCannonDruidScript.gd
+++ Invocations/CannonDruid/BaseCannonDruidScript.gd
@@ -4,14 +4,4 @@ class_name CannonUnitScript
 
 func _ready():
 	super()
-#	initAttacks()
-#	initStats()
-#
-#func initAttacks():
-#	attacks = {
-#	"cannon shot" : preload("res://Invocations/CannonDruid/Attacks/CannonShot/Cannonshot.tres").duplicate(true),
-#	"rebuild":preload("res://Invocations/CannonDruid/Attacks/Rebuild/Rebuild.tres").duplicate(true)
-#	}
-#
-#func initStats():
-#	pass
+
--- Invocations/Samurai/BaseSamuraiScript.gd
+++ Invocations/Samurai/BaseSamuraiScript.gd
@@ -3,14 +3,4 @@ class_name BaseSamuraiScript
 
 func _ready():
 	super()
-#	initAttacks()
-#	initStats()
 
-func initAttacks():
-#	attacks = {
-#	"sword slash" : preload("res://Invocations/Samurai/Attacks/Sword slash/SwordSlash.tres").duplicate(true),
-#	"sword slam": preload("res://Invocations/Samurai/Attacks/Sword slam/SwordSlam.tres").duplicate(true),
-#}
-
-#func initStats():
-#	pass
